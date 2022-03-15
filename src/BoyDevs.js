f574a2ef81e9c25d58e0e8e2f04084cd10ae895e
Katrina <Krmoore014@gmail.com>
2022-03-15 15:59:36 -0500

    installed react-icons

--- src/BoyDevs.js
+++ src/BoyDevs.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const BoyDevs = ({id}) => {
+const BoyDevs = () => {
     return (
         <div className="flex flex-column">
             <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
--- src/GirlDevs.js
+++ src/GirlDevs.js
@@ -1,12 +1,14 @@
 import React from "react";
+import { GoMarkGithub } from "react-icons/go";
 
-const GirlDevs = ({id}) => {
+const GirlDevs = () => {
     return (
         <div className="flex flex-column">
             <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
                 <img alt="robots" src={` https://robohash.org/set_set2/bgset_any/Katrina?size=200x200`} />
                 <div>
                     <h2>Katrina</h2>
+                    <GoMarkGithub />
                 </div>
             </div>
             <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
