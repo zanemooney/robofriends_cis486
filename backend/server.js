c490f7f780ab96f3b898722a97e1743c65361d4f
mjohnson7cd <mjohnson7cd@gmail.com>
2022-03-16 10:50:37 -0500

    Refactored backend code

--- backend/server.js
+++ backend/server.js
@@ -10,42 +10,20 @@ MongoClient.connect(uri, { useUnifiedTopology: true })
     const db = client.db('robofriends')
     const robosCollection = db.collection('robos')
 
-
-      
-  
-      
-      app.put('/robos-up/:id', (req, res) => { 
-        robosCollection.findOneAndUpdate(req.params)
+      app.put('/robos-up/:apiID', (req, res) => { 
+        res = robosCollection.findOneAndUpdate(req.params, req.body).json
           .catch(error => console.error(error))
-      })
+      });
 
-      app.put('/robos-down/:id', (req, res) => { 
-        robosCollection.findOneAndUpdate(req.params)
+      app.put('/robos-down/:apiID', (req, res) => { 
+        res = robosCollection.findOneAndUpdate(req.params, req.body).json
           .catch(error => console.error(error))
-      })
+      });
 
-//multi endpoints...specific to what you are doing
-//don't have to cram it all in just one get, put, delete, ...
-
-//app.put('robo-up/:id,
-
-//app.put('robo-down/:id
-
-
-
-      app.get('/robos/:id', (req, res) => { 
-        robosCollection.find(req.params)
+      app.get('/robos/:apiID', (req, res) => { 
+        res = (robosCollection.find(req.params)).json
           .catch(error => console.error(error))
-      })
-
-      // app.get('/robos', (req, res) => { 
-      //   const votes = robosCollection.find().toArray
-      //   res.send(votes.reduce((a, b) => a+b) / votes.length)
-      //     .catch(error => console.error(error))
-      // })
-  
-      
-      
+      });
 
 
     // ========================
