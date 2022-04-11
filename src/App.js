107cf7cef81907b0ea03c51424679a889da5b212
Katrina <Krmoore014@gmail.com>
2022-04-11 17:49:06 -0500

    put it back for later

--- src/App.js
+++ src/App.js
@@ -21,6 +21,9 @@ class App extends Component {
             .then(users => this.setState({robots: users})); 
     }
 
+    onSearchChange = (event) => {
+        this.setState({searchfield: event.target.value})
+    }
 
     render() {
         const filteredRobots = this.state.robots.filter(robots => {
@@ -32,6 +35,7 @@ class App extends Component {
             return (
                 <div className="tc">        
                     <h1 className="f1" >RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
                     <h2>Vote for you favorite Robo Friend!</h2>
                     <section className="flex justify-around mr2">
                         <GirlDevs />
