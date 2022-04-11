70e3058a912a9eccfcde2b1685e05a27efa93f08
Katrina <Krmoore014@gmail.com>
2022-04-11 17:40:07 -0500

    got rid of search

--- src/App.js
+++ src/App.js
@@ -21,9 +21,6 @@ class App extends Component {
             .then(users => this.setState({robots: users})); 
     }
 
-    onSearchChange = (event) => {
-        this.setState({searchfield: event.target.value})
-    }
 
     render() {
         const filteredRobots = this.state.robots.filter(robots => {
@@ -35,7 +32,6 @@ class App extends Component {
             return (
                 <div className="tc">        
                     <h1 className="f1" >RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange} />
                     <h2>Vote for you favorite Robo Friend!</h2>
                     <section className="flex justify-around mr2">
                         <GirlDevs />
